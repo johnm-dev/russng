f9b7c6148f967e053da4e331aa7d2a3128b76cc7
John Marshall <John.Marshall@ec.gc.ca>
2012-03-21 09:14:36 -0400

    Move new rudial.c to rudial2.c (temporarily); put back old rudial.c into place

--- russtools/src/bin/rudial.c
+++ russtools/src/bin/rudial.c
@@ -1,29 +1,32 @@
 /*
-* rudial.c
+** rudial.c
+**
+** front-end for russ services
 */
 
 /*
-# license--start
+# GPL--start
+# This file is part of russ
+# Copyright (C) 2011 Environment/Environnement Canada
 #
-# This file is part of RUSS tools.
-# Copyright (C) 2012 John Marshall
-#
-# RUSS tools is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License.
+# 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# license--end
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# GPL--end
 */
 
+#include <dirent.h>
+#include <errno.h>
 #include <libgen.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -33,202 +36,242 @@
 
 #include "russ.h"
 
-struct streamer_struct {
-	pthread_t	th;
-	int		in_fd, out_fd;
-	int		count, blocksize;
-};
+#define ERROR_DIAL_LOCAL	120
+#define ERROR_DIAL_CALL	121
 
-static void *
-_streamer_thread(void *v) {
-	struct streamer_struct	*ss;
+int	forwarding = 1;
 
-	ss = v;
-	russ_stream_fd(ss->in_fd, ss->out_fd, ss->count, ss->blocksize);
+int
+list_dir(char *path) {
+	DIR		*dir;
+	struct dirent	*dirent;
+
+	if ((dir = opendir(path)) == NULL) {
+		return(-1);
+	}
+
+	while ((dirent = readdir(dir)) != NULL) {
+		if (strncmp(dirent->d_name, ".", 1) != 0) {
+			printf("%s\n", dirent->d_name);
+		}
+	}
+	closedir(dir);
+	return 0;
+}
+
+/* forward bytes between fds */
+void
+__forward_bytes(int *fds, int how) {
+	char	buf[1<<20];
+	int	rcount, wcount;
+	int	src_fd, dst_fd, close_fd;
+
+	/* unpack */
+	src_fd = ((int *)fds)[0];
+	dst_fd = ((int *)fds)[1];
+	close_fd = ((int *)fds)[2];
+
+	while (1) {
+		if (how == 0) {
+			rcount = russ_readn(src_fd, buf, sizeof(buf));
+		} else {
+			rcount = russ_readline(src_fd, buf, sizeof(buf));
+		}
+		if (rcount <= 0) {
+			/* EOF or error */
+			break;
+		}
+		wcount = russ_writen(dst_fd, buf, rcount);
+		if (wcount != rcount) {
+			/* error */
+			break;
+		}
+		fsync(dst_fd);
+	}
+	if (close_fd > -1) {
+		close(close_fd);
+	}
+	free(fds);
+}
+
+/* forward bytes between fds (pthread-friendly) */
+void *
+forward_bytes(void *fds) {
+	__forward_bytes(fds, 0);
 	return NULL;
 }
 
-static struct streamer_struct *
-_stream_bytes(int in_fd, int out_fd, int count, int blocksize) {
-	struct streamer_struct	*ss;
+/* forward lines between fds (pthread-friendly) */
+void *
+forward_lines(void *fds) {
+	__forward_bytes(fds, 1);
+	return NULL;
+}
 
-	if ((ss = malloc(sizeof(struct streamer_struct))) == NULL) {
-		return NULL;
+/* spawn thread to forward bytes */
+int
+spawn_forward_bytes(pthread_t *th, int src_fd, int dst_fd, int close_fd) {
+	int	*fds;
+
+	if ((fds = malloc(sizeof(int)*3)) == NULL) {
+		return -1;
 	}
-	ss->in_fd = in_fd;
-	ss->out_fd = out_fd;
-	ss->count = count;
-	ss->blocksize = blocksize;
-	if (pthread_create(&(ss->th), NULL, _streamer_thread, (void *)ss) < 0) {
-		free(ss);
-		return NULL;
-	}
-	return ss;
+	fds[0] = src_fd;
+	fds[1] = dst_fd;
+	fds[2] = close_fd;
+	return pthread_create(th, NULL, forward_lines, (void *)fds);
 }
 
 void
-print_usage(char *prog_name) {
-	if (strcmp(prog_name, "ruhelp") == 0) {
-		fprintf(stderr, 
+print_usage(char **argv) {
+	char	*name, *bname;
+
+	name = strdup(argv[0]);
+	bname = basename(name);
+
+	if (strcmp(bname, "ruhelp") == 0) {
+		fprintf(stderr,
 "usage: ruhelp <addr>\n"
 "\n"
-"Alias for rudial -h ...\n");
-	} else if (strcmp(prog_name, "ruinfo") == 0) {
+"An alias for 'rudial -h ...'.\n");
+
+	} else if (strcmp(bname, "ruinfo") == 0) {
 		fprintf(stderr,
-"usage: ruinfo <addr>\n"
+"usage: ruinfo -i <addr>\n"
 "\n"
-"Alias for rudial -i ...\n");
-	} else if (strcmp(prog_name, "ruls") == 0) {
+"An alias for 'rudial -i ...'.\n");
+
+	} else if (strcmp(bname, "ruls") == 0) {
 		fprintf(stderr,
-"usage: ruls <addr>\n",
+"usage: ruls [<addr>]\n"
 "\n"
-"Alias for rudial -l ...\n");
-	} else if (strcmp(prog_name, "rudial") == 0) {
+"An alias for 'rudial -l ...'.\n");
+
+	} else {
 		fprintf(stderr,
 "usage: rudial [<option>] <addr> [<arg> ...]\n"
 "\n"
-"Dial service at <addr>. A service may support one or more\n"
-"operations (e.g., execute, list); execute is the default operation.\n"
-"Most other services are for obtaining state information.\n"
+"Dial service at <addr> with given arguments.\n"
 "\n"
-"A successful dial will effectively connect the stdin, stdout, and\n"
-"stderr of the service to that of the rudial. I.e., input to stdin\n"
-"of rudial will be forwarded to the service, stdout and stderr of\n"
-"the service will be forwarded to the same of rudial.\n"
-"\n"
-"An exit value of < 0 indicates a failure to connect. Otherwise a 0\n"
-"exit value is returned.\n"
-"\n"
-"Options:\n"
-"-h|--help      print this information\n"
-"-i|--info      show server-specific information as zero or more\n"
-"               name=value lines\n"
-"-l|--list      list services provided by the server as paths to be\n"
-"               used in the <addr> argument\n"
-"-o|--op <op>   request operation <op> from service\n"
+"Where <option> is:\n"
+"--execute	Execute service request (shortcut for '--op execute';\n"
+"		default	if <option> is not given).\n"
+"--id		Return unique server id.\n"
+"-h|--help	Show help for the (sub)service.\n"
+"-i|--info	Show name=value information for server.\n"
+"-l|--list	List (sub)service(s) (shortcut for '--op list').\n"
+"-o|--op <op>	Use operation name (e.g., execute) but not supported\n"
+"		otherwise.\n"
 "-t|--timeout <seconds>\n"
-"               quit the dial if it does not connect within the\n"
-"               given time\n");
+"		Allow timeout seconds to complete connection. Default\n"
+"		is no timeout.\n"
+"\n"
+"Exit values of %d (local error) and %d (call error) are\n"
+"reserved by rudial.\n", ERROR_DIAL_LOCAL, ERROR_DIAL_CALL);
 	}
+	free(name);
 }
 
 int
 main(int argc, char **argv) {
-	struct russ_conn	*conn;
-	struct streamer_struct	*ss_in, *ss_out, *ss_err;
 	char			*prog_name;
-	char			*op, *addr;
-	char			*arg, **args;
-	char			*attrv[RUSS_MAX_ATTRC];
+	struct russ_conn	*conn;
+	char			*addr, *saddr, *spath, *op;
+	int			exit_value;
+	pthread_t		in_th, out_th, err_th;
+	int			pass_fds[3];
+	struct pipe_fds		pipes[3];
 	int			argi;
-	int			attrc;
+	int			line_buffering;
 	int			timeout;
 
 	prog_name = basename(strdup(argv[0]));
-	if (argc < 2) {
-		fprintf(stderr, "error: bad/missing arguments\n");
-		exit(-1);
+
+	if ((argc < 2) && (strcmp(prog_name, "ruls") != 0)) {
+		print_usage(argv);
+		exit(ERROR_DIAL_LOCAL);
 	}
 
-	/* aliases */
 	if (strcmp(prog_name, "ruhelp") == 0) {
 		op = "help";
+		argi = 1;
 	} else if (strcmp(prog_name, "ruinfo") == 0) {
 		op = "info";
+		argi = 1;
 	} else if (strcmp(prog_name, "ruls") == 0) {
 		op = "list";
-	} else if (strcmp(prog_name, "rudial") == 0) {
-		op = "execute";
+		argi = 1;
 	} else {
-		fprintf(stderr, "error: unknown alias\n");
-		exit(-1);
-	}
-
-	/* options */
-	argi = 1;
-	attrc = 0;
-	while (argi < argc) {
-		arg = argv[argi++];
-
-		if (strncmp(arg, "-", 1) != 0) {
-			argi--;
-			break;
+		/* TODO: arg parsing does not handle multiple options properly */
+		/* rudial */
+		timeout = -1;
+		if ((strcmp(argv[1], "--op") == 0)
+			|| (strcmp(argv[1], "-o") == 0)) {
+			if (argc < 3) {
+				fprintf(stderr, "error: missing op\n");
+				exit(ERROR_DIAL_LOCAL);
+			} else {
+				op = argv[2];
+			}
+			argi = 3;
+		} else if (strncmp(argv[1], "-", 1) == 0) {
+			if (strcmp(argv[1], "--execute") == 0) {
+				op = "execute";
+			} else if ((strcmp(argv[1], "--help") == 0)
+				|| (strcmp(argv[1], "-h") == 0)) {
+				op = "help";
+			} else if ((strcmp(argv[1], "--info") == 0)
+				|| (strcmp(argv[1], "-i") == 0)) {
+				op = "info";
+			} else if (strcmp(argv[1], "--id") == 0) {
+				op = "id";
+			} else if ((strcmp(argv[1], "--list") == 0)
+				|| (strcmp(argv[1], "-l") == 0)) {
+				op = "list";
+			} else if (((strcmp(argv[1], "--timeout") == 0) || (strcmp(argv[1], "-t")))
+					&& (argc > 1)) {
+				if (sscanf(argv[1], "%d", &timeout) < 0) {
+					fprintf(stderr, "error: bad timeout value\n");
+					exit(-1);
+				}
+			} else {
+				fprintf(stderr, "error: unknown op (%s)\n", argv[1]);
+				exit(ERROR_DIAL_CALL);
+			}
+			argi = 2;
+		} else {
+			op = "execute";
+			argi = 1;
 		}
-		if ((strcmp(arg, "--help") == 0)
-			|| (strcmp(arg, "-h") == 0)) {
-
-			print_usage(prog_name);
-			exit(0);
-		} else if (strcmp(arg, "--id") == 0) {
-
-			op = "id";
-			continue;
-		} else if ((strcmp(arg, "--info") == 0)
-			|| (strcmp(arg, "-i") == 0)) {
-
-			op = "info";
-			continue;
-		} else if ((strcmp(arg, "--list") == 0)
-			|| (strcmp(arg, "-l") == 0)) {
-
-			op = "list";
-			continue;
-		} else if (((strcmp(arg, "--op") == 0) || (strcmp(arg, "-o") == 0))
-			&& (argi < argc)) {
-			
-			arg = argv[argi++];
-			op = arg;
-			continue;
-		} else if (((strcmp(arg, "--timeout") == 0) || (strcmp(arg, "-t") == 0))
-			&& (argi < argc)) {
-
-			arg = argv[argi++];
-			if (sscanf(argv[argi], "%d", &timeout) >= 0) {
-				continue;
-			}
-		} else if (((strcmp(arg, "--attr") == 0) || (strcmp(arg, "-a") == 0))
-			&& (argi < argc)) {
-			
-			arg = argv[argi++];
-			if (attrc >= RUSS_MAX_ATTRC-1) {
-				fprintf(stderr, "error: too many attributes\n");
-				exit(-1);
-			}
-			if (strstr(arg, "=") == NULL) {
-				fprintf(stderr, "error: bad attribute format\n");
-				exit(-1);
-			}
-			attrv[attrc++] = arg;
-			attrv[attrc] = NULL;
-		}
-		fprintf(stderr, "error: bad/missing arguments\n");
+	}
+	if (argi >= argc) {
+		print_usage(argv);
 		exit(-1);
 	}
 
-	/* addr and args */
-	if (argi+1 >= argc) {
-		fprintf(stderr, "error: bad/missing arguments\n");
-		exit(-1);
-	}
-	addr = argv[argi++];
-	args = &(argv[argi]);
-	if ((conn = russ_dialv(addr, op, timeout, NULL, argc-argi, args)) == NULL) {
+	addr = argv[argi];
+	argi++;
+
+	if ((conn = russ_dialv(addr, op, timeout, NULL, argc-argi, &argv[argi])) == NULL) {
 		fprintf(stderr, "error: cannot dial service\n");
 		exit(-1);
 	}
 
-	/* stream bytes between fds */
-	if (((ss_in = _stream_bytes(STDIN_FILENO, conn->fds[0], -1, 16384)) == NULL)
-		|| ((ss_out = _stream_bytes(STDOUT_FILENO, conn->fds[1], -1, 16384)) == NULL)
-		|| ((ss_err = _stream_bytes(STDERR_FILENO, conn->fds[2], -1, 16384)) == NULL)) {
-		fprintf(stderr, "error: failed to stream\n");
+	if ((spawn_forward_bytes(&in_th, STDIN_FILENO, conn->fds[0], conn->fds[0]) < 0)
+		|| (spawn_forward_bytes(&out_th, conn->fds[1], STDOUT_FILENO, conn->fds[1]) < 0)
+		|| (spawn_forward_bytes(&err_th, conn->fds[2], STDERR_FILENO, conn->fds[2]) < 0)) {
+		fprintf(stderr, "error: failed to start thread for forwarder\n");
 		exit(-1);
 	}
 
-	pthread_join(ss_out->th, NULL);
-	pthread_join(ss_err->th, NULL);
+	dprintf("waiting...\n");
+	pthread_join(out_th, NULL);
+	pthread_join(err_th, NULL);
+	/* don't join in_th because a closed out and err mean the end */
+	/*pthread_join(in_th, NULL);*/
 
 	russ_close_conn(conn);
 	conn = russ_free_conn(conn);
+
 	exit(0);
 }
