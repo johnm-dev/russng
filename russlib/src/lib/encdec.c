766e230c23634a9cda560f2632389b6f147ac18e
John Marshall <johnm.dev@gmail.com>
2012-03-22 10:15:00 -0400

    Move encdec2.c into place of encdec.c

--- russlib/src/lib/encdec.c
+++ russlib/src/lib/encdec.c
@@ -1,10 +1,5 @@
 /*
-* russ_encdec.c
-*
-* encode and decode into/from byte strings.
-*
-* Encoding is in big-endian. Strings and byte strings are encoded
-* with size(4b) then the string/bytes.
+* encdec2.c
 */
 
 /*
@@ -34,107 +29,251 @@
 #include <stdlib.h>
 #include <string.h>
 
-uint16_t
-russ_dec_H(char *b, int *count) {
-	/* printf("(%hhu) (%hhu)\n", b[0], b[1]); */
-	*count = 2;
-	return (uint8_t)(b[0])
-		| ((uint8_t)(b[1])<<8);
-}
-
-uint32_t
-russ_dec_I(char *b, int *count) {
-	/* printf("(%hhu) (%hhu) (%hhu) (%hhu)\n", b[0], b[1], b[2], b[3]); */
-	*count = 4;
-	return (uint8_t)(b[0])
-		| ((uint8_t)(b[1])<<8)
-		| ((uint8_t)(b[2])<<16)
-		| ((uint8_t)(b[3])<<24);
-}
-
-int32_t
-russ_dec_i(char *b, int *count) {
-	/* printf("(%hhu) (%hhu) (%hhu) (%hhu)\n", b[0], b[1], b[2], b[3]); */
-	*count = 4;
-	return (uint8_t)b[0]
-		| ((uint8_t)(b[1])<<8)
-		| ((uint8_t)(b[2])<<16)
-		| ((uint8_t)(b[3])<<24);
-}
+#include "russ_priv.h"
 
 char *
-russ_dec_b(char *b, int *count) {
-	char	*dst;
-	int	_count;
-
-	_count = russ_dec_I(b, count);
-	b += *count;
-	if (dst = malloc(_count)) {
-		memcpy(dst, b, _count);
-		*count += _count;
-	}
-	return dst;
-}
-
-char *
-russ_dec_s(char *b, int *count) {
-	return russ_dec_b(b, count);
-}
-
-char *
-russ_enc_H(char *b, uint16_t v, int buf_size) {
-	if (buf_size < 2) {
-		return NULL;
-	}
-
-	b[0] = (uint8_t)v & 0xff;
-	b[1] = (uint8_t)(v >> 8) & 0xff;
-	/* printf("(%hu) (%hhu) (%hhu)\n", v, b[0], b[1]); */
+russ_dec2_H(char *b, uint16_t *v) {
+	*v = (uint8_t)(b[0]) | ((uint8_t)(b[1])<<8);
 	return b+2;
 }
 
 char *
-russ_enc_I(char *b, uint32_t v, int buf_size) {
-	if (buf_size < 4) {
-		return NULL;
-	}
-
-	b[0] = (uint8_t)v & 0xff;
-	b[1] = (uint8_t)(v >> 8) & 0xff;
-	b[2] = (uint8_t)(v >> 16) & 0xff;
-	b[3] = (uint8_t)(v >> 24) & 0xff;
-	/* printf("(%hu) (%hhu) (%hhu) (%hhu) (%hhu)\n", v, b[0], b[1], b[2], b[3]); */
+russ_dec2_I(char *b, uint32_t *v) {
+	*v = (uint8_t)(b[0]) | ((uint8_t)(b[1])<<8)
+		| ((uint8_t)(b[2])<<16) | ((uint8_t)(b[3])<<24);
 	return b+4;
 }
 
 char *
-russ_enc_i(char *b, int32_t v, int buf_size) {
-	if (buf_size < 4) {
-		return NULL;
-	}
+russ_dec2_i(char *b, int32_t *v) {
+	uint32_t	v2;
+	char		*b2;
 
-	b[0] = (uint8_t)v & 0xff;
-	b[1] = (uint8_t)(v >> 8) & 0xff;
-	b[2] = (uint8_t)(v >> 16) & 0xff;
-	b[3] = (uint8_t)(v >> 24) & 0xff;
-	/* printf("(%hu) (%hhu) (%hhu) (%hhu) (%hhu)\n", v, b[0], b[1], b[2], b[3]); */
-	return b+4;
+	b2 = russ_dec2_I(b, &v2);
+	*v = (int32_t)v2;
+	return b2;
 }
 
-/* encoding: size(4b) bytes(n) */
 char *
-russ_enc_bytes(char *b, char *s, int count, int buf_size) {
-	if (buf_size >= 4+count) {
-		b = russ_enc_I(b, count, buf_size);
-		memcpy(b, s, count);
-		return b+count;
+russ_dec2_b(char *b, char **bp) {
+	int	count;
+
+	if (((b = russ_dec2_I(b, &count)) == NULL)
+		|| ((*bp = malloc(count)) == NULL)) {
+		return NULL;
+	}
+	memcpy(*bp, b, count);
+	return b+count;
+}
+
+char *
+russ_dec2_s(char *b, char **bp) {
+	return russ_dec2_b(b, bp);
+}
+
+/**
+* Shared function for decoding string arrays.
+*/
+static char *
+_dec2_s_array0(char *b, char ***v, int *alen, int append_null) {
+	char	**array, *s;
+	int	_bcount, i;
+
+	b = russ_dec2_I(b, alen);
+	if (*alen > 0) {
+		if (append_null) {
+			array = malloc(sizeof(char *)*(*alen+1));
+		} else {
+			array = malloc(sizeof(char *)*(*alen));
+		}
+		if (array == NULL) {
+			return NULL;
+		}
+		for (i = 0; i < *alen; i++) {
+			b = russ_dec2_s(b, &s);
+			if (b == NULL) {
+				goto free_array;
+			}
+			array[i] = s;
+		}
+		if (append_null) {
+			array[*alen] = NULL;
+		}
 	} else {
-		return b;
+		array = NULL;
 	}
+	*v = array;
+	return b;
+free_array:
+	for (; i >= 0; i--) {
+		free(array[i]);
+	}
+	free(*array);
+	return NULL;
 }
 
+/**
+* Decode string array with implicit NULL sentinel.
+*
+* @param b	buffer
+* @param[out] vpp	string array with NULL sentinel
+* @return	new buffer position; NULL if failure
+*/
 char *
-russ_enc_string(char *b, char *s, int buf_size) {
-	return russ_enc_bytes(b, s, strlen(s)+1, buf_size);
+russ_dec2_s_array0(char *b, char ***vpp, int *alen) {
+	return _dec2_s_array0(b, vpp, alen, 1);
 }
 
+/**
+* Decode string array of fixed # of items.
+*
+* @param b	buffer
+* @param[out] vpp	string array with NULL sentinel
+* @return	new buffer position; NULL if failure
+*/
+char *
+russ_dec2_s_arrayn(char *b, char ***vpp, int *alen) {
+	return _dec2_s_array0(b, vpp, alen, 0);
+}
+
+/***** encoders *****/
+
+/**
+* Encode uint16.
+*
+* @param b	buffer
+* @param bend	end of buffer
+* @param v	uint16 value
+* @return	new buffer position; NULL if failure
+*/
+char *
+russ_enc2_H(char *b, char *bend, uint16_t v) {
+	if ((bend-b) < 2) {
+		return NULL;
+	}
+	b[0] = (uint8_t)v & 0xff;
+	b[1] = (uint8_t)(v >> 8) & 0xff;
+	return b+2;
+}
+
+/**
+* Encode uint32.
+*
+* @param b	buffer
+* @param bend	end of buffer
+* @param v	uint32 value
+* @return	new buffer position; NULL if failure
+*/
+char *
+russ_enc2_I(char *b, char *bend, uint32_t v) {
+	if ((bend-b) < 4) {
+		return NULL;
+	}
+	b[0] = (uint8_t)v & 0xff;
+	b[1] = (uint8_t)(v>>8) & 0xff;
+	b[2] = (uint8_t)(v>>16) & 0xff;
+	b[3] = (uint8_t)(v>>24) & 0xff;
+	return b+4;
+}
+
+/**
+* Encode int32.
+*
+* @param b	buffer
+* @param bend	end of buffer
+* @param v	int32 value
+* @return	new buffer position; NULL if failure
+*/
+char *
+russ_enc2_i(char *b, char *bend, int32_t v) {
+	if ((bend-b) < 4) {
+		return NULL;
+	}
+	b[0] = (uint8_t)v & 0xff;
+	b[1] = (uint8_t)(v >> 8) & 0xff;
+	b[2] = (uint8_t)(v >> 16) & 0xff;
+	b[3] = (uint8_t)(v >> 24) & 0xff;
+	return b+4;
+}
+
+/**
+* Encode byte array.
+* Encoding: alen(4) bytes(n)
+*
+* @param b	buffer
+* @param bend	end of buffer
+* @param v	array of bytes
+* @return	new buffer position; NULL if failure
+*/
+char *
+russ_enc2_bytes(char *b, char *bend, char *v, int alen) {
+	if ((bend-b) < 4+alen) {
+		return NULL;
+	}
+	b = russ_enc2_I(b, bend, alen);
+	memcpy(b, v, alen);
+	return b+alen;
+}
+
+/**
+* Encode string (including \0).
+* Encoding: alen(4) string(n)
+*
+* @param b	buffer
+* @param bend	end of buffer
+* @param v	string ending in \0
+* @return	new buffer position; NULL if failure
+*/
+char *
+russ_enc2_string(char *b, char *bend, char *v) {
+	return russ_enc2_bytes(b, bend, v, strlen(v)+1);
+}
+
+/**
+* Encode string array of fixed length.
+* Encoding: alen(4b) [string [...]]
+*
+* @param b	buffer
+* @param bend	end of buffer
+* @param v	array of strings (ignored if alen==0)
+* @param alen	# of array items
+* @return	updated buffer position; NULL on failure
+*/
+char *
+russ_enc2_s_arrayn(char *b, char *bend, char **v, int alen) {
+	int	i;
+
+	if ((b = russ_enc2_I(b, bend, alen)) == NULL) {
+		return NULL;
+	}
+	for (i = 0; i < alen; i++) {
+		if ((b = russ_enc2_string(b, bend, v[i])) == NULL) {
+			return NULL;
+		}
+	}
+	return b;
+}
+
+/**
+* Encode string array having NULL sentinel. Calls russ_enc_s_arrayn.
+* Sentinel is not encoded.
+*
+* @param b	buffer
+* @param bend	end of buffer
+* @param v	array of strings (may also be NULL)
+* @return	updated buffer position; NULL on failure
+*/
+char *
+russ_enc2_s_array0(char *b, char *bend, char **v) {
+	int	alen;
+
+	if (v == NULL) {
+		alen = 0;
+	} else {
+		for (alen = 0; (alen < 16384) && (v[alen] != NULL); alen++);
+		if (alen == 16384) {
+			return NULL;
+		}
+	}
+	return russ_enc2_s_arrayn(b, bend, v, alen);
+}
