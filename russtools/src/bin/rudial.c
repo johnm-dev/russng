df312785d59747e51fc426c041c9ea0ec18295e4
John Marshall <John.Marshall@ec.gc.ca>
2012-06-03 07:55:08 -0400

    Use russ_conn_wait to wait on service exit

--- russtools/src/bin/rudial.c
+++ russtools/src/bin/rudial.c
@@ -73,7 +73,7 @@ print_usage(char *prog_name) {
 int
 main(int argc, char **argv) {
 	struct russ_conn	*conn;
-	struct russ_forwarder	fwds[RUSS_CONN_NFDS+1];
+	struct russ_forwarder	fwds[RUSS_CONN_NFDS];
 	russ_timeout		timeout;
 	char			*prog_name;
 	char			*op, *addr;
@@ -82,6 +82,7 @@ main(int argc, char **argv) {
 	int			argi;
 	int			attrc;
 	int			req_opt_mask;
+	int			exit_status;
 
 	prog_name = basename(strdup(argv[0]));
 
@@ -157,11 +158,11 @@ main(int argc, char **argv) {
 	russ_forwarder_init(&(fwds[0]), 0, STDIN_FILENO, conn->fds[0], -1, 16384, 0);
 	russ_forwarder_init(&(fwds[1]), 0, conn->fds[1], STDOUT_FILENO, -1, 16384, 0);
 	russ_forwarder_init(&(fwds[2]), 0, conn->fds[2], STDERR_FILENO, -1, 16384, 0);
-	russ_forwarder_init(&(fwds[3]), 1, conn->exit_fd, -1, -1, 16384, 0);
-	if (russ_run_forwarders(RUSS_CONN_NFDS+1, fwds) < 0) {
+	if (russ_run_forwarders(RUSS_CONN_NFDS, fwds) < 0) {
 		fprintf(stderr, "error: could not forward bytes\n");
 		exit(-1);
 	}
+	russ_conn_wait(conn, &exit_status, NULL, -1);
 
 	russ_conn_close(conn);
 	conn = russ_conn_free(conn);
