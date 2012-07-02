645510b408d578c2c39c26958bf87f1b582f4d01
John Marshall <John.Marshall@ec.gc.ca>
2012-07-01 20:55:09 -0400

    Update programs that use forwarders

--- russtools/src/usr/bin/rudial.c
+++ russtools/src/usr/bin/rudial.c
@@ -154,17 +154,25 @@ main(int argc, char **argv) {
 		exit(-1);
 	}
 
-	russ_forwarder_init(&(fwds[0]), STDIN_FILENO, conn->fds[0], -1, 16384, 0);
-	russ_forwarder_init(&(fwds[1]), conn->fds[1], STDOUT_FILENO, -1, 16384, 0);
-	russ_forwarder_init(&(fwds[2]), conn->fds[2], STDERR_FILENO, -1, 16384, 0);
+	/* initialize forwarders (handing off fds) and start threads */
+	russ_forwarder_init(&(fwds[0]), STDIN_FILENO, conn->fds[0], -1, 16384, 0, 1);
+	russ_forwarder_init(&(fwds[1]), conn->fds[1], STDOUT_FILENO, -1, 16384, 0, 1);
+	russ_forwarder_init(&(fwds[2]), conn->fds[2], STDERR_FILENO, -1, 16384, 0, 1);
+	conn->fds[0] = -1;
+	conn->fds[1] = -1;
+	conn->fds[2] = -1;
 	if (russ_run_forwarders(conn->nfds, fwds) < 0) {
 		fprintf(stderr, "error: could not forward bytes\n");
 		exit(-1);
 	}
-	if (russ_conn_wait(conn, &exit_status, -1) < 0) {
+
+	/* wait for exit */
+	if ((rv = russ_conn_wait(conn, &exit_status, -1)) < 0) {
 		exit_status = -127;
 	}
+
 	russ_forwarder_join(&(fwds[1]));
+	russ_forwarder_join(&(fwds[2]));
 
 	russ_conn_close(conn);
 	conn = russ_conn_free(conn);
