81f5f3bd4af5bad6ce0b67dabb10cb9f476ff05a
John Marshall <John.Marshall@ec.gc.ca>
2012-07-18 00:02:08 -0400

    Enhancements to ssh-based server.
    New services using hostsfile to support indexed host (/hid),
    user@host validation (/host), and generic userhost with (/net).

--- servers/src/usr/bin/rusrv_ssh.c
+++ servers/src/usr/bin/rusrv_ssh.c
@@ -37,49 +37,65 @@ extern char **environ;
 #include "configparser.h"
 #include "russ.h"
 
+#define MAX_HOSTS	(1024)
+
+struct hostslist {
+	char	*hosts[MAX_HOSTS];
+	int	nhosts;
+};
+
 /* global */
-struct configparser	*config;
+struct configparser	*config = NULL;
+char			*hostsfilename = NULL;
+struct hostslist	hostslist;
 
 char	*HELP = 
 "Provides access to remote user@host using ssh.\n"
 "\n"
-"/user@host/... <args>\n"
+"hid/<hid>/... <args>\n"
+"    Connect to service ... at user@host identified by a lookup\n"
+"    into the hostsfile list where <hid> is the index. Only\n"
+"    available if a hostsfile was given at startup.\n"
+"\n"
+"host/<user@host>/... <args>\n"
+"    Connect to service ... at user@host verified by a lookup into\n"
+"    the hostsfile list. Only available if a hostsfile was given\n"
+"    at startup.\n"
+"\n"
+"net/user@host/... <args>\n"
 "    Connect to service ... at user@host using ssh.\n";
 
 #define SSH_EXEC	"/usr/bin/ssh"
 #define RUDIAL_EXEC	"/usr/bin/rudial"
 
-void
-svc_ssh_handler(struct russ_conn *conn) {
-	char	*args[1024];
-	int	nargs;
-	char	*p, *new_spath, *userhost;
+int
+switch_user(struct russ_conn *conn) {
 	uid_t	uid;
 	gid_t	gid;
-	int	i, n, status, pid;
 
 	uid = conn->cred.uid;
 	gid = conn->cred.gid;
 
 	if (uid == 0) {
-		russ_dprintf(conn->fds[2], "error: cannot run for root (uid of 0)\n");
-		russ_conn_exit(conn, -1);
+		russ_conn_fatal(conn, "error: cannot run for root (uid of 0)", -1);
 		exit(0);
 	}
 
 	/* switch user */
-	setgid(gid);
-	setuid(uid);
-
-	/* extract and validate user@host and new_spath */
-	userhost = &conn->req.spath[1];
-	if ((p = index(userhost, '/')) == NULL) {
-		russ_dprintf(conn->fds[2], "error: cannot run for root (uid of 0)\n");
-		russ_conn_exit(conn, -1);
+	if ((setgid(gid) < 0) || (setuid(uid) < 0)) {
+		russ_conn_fatal(conn, "error: cannot switch user", -1);
 		exit(0);
 	}
-	new_spath = strdup(p);
-	p[0] = '\0'; /* terminate userhost */
+	return 0;
+}
+
+void
+execute(struct russ_conn *conn, char *userhost, char *new_spath) {
+	char	*args[1024];
+	int	nargs;
+	int	i, status, pid;
+
+	switch_user(conn);
 
 	/* build args array */
 	nargs = 0;
@@ -119,26 +135,131 @@ svc_ssh_handler(struct russ_conn *conn) {
 	close(conn->fds[2]);
 	waitpid(pid, &status, 0);
 
-	fprintf(stderr, "status (%d) exit status (%d)\n", status, WEXITSTATUS(status));
 	russ_conn_exit(conn, WEXITSTATUS(status));
 	russ_conn_close(conn);
 	exit(0);
 }
 
 void
+svc_hid_handler(struct russ_conn *conn) {
+	char	*p, *new_spath, *hids, *userhost;
+	int	i, hid;
+
+	/* extract and validate user@host and new_spath */
+	hids = &conn->req.spath[5];
+	if ((p = index(hids, '/')) == NULL) {
+		russ_conn_fatal(conn, RUSS_MSG_NO_SERVICE, RUSS_EXIT_FAILURE);
+		exit(0);
+	}
+	new_spath = strdup(p);
+	p[0] = '\0'; /* terminate userhost */
+	if (sscanf(hids, "%d", &hid) < 0) {
+		russ_conn_fatal(conn, RUSS_MSG_NO_SERVICE, RUSS_EXIT_FAILURE);
+		exit(0);
+	}
+	if ((hid < 0) || (hid >= hostslist.nhosts)) {
+		russ_conn_fatal(conn, RUSS_MSG_NO_SERVICE, RUSS_EXIT_FAILURE);
+		exit(0);
+	}
+	userhost = hostslist.hosts[hid];
+
+	execute(conn, userhost, new_spath);
+}
+
+void
+svc_host_handler(struct russ_conn *conn) {
+	char	*p, *new_spath, *userhost;
+	int	i;
+
+	/* extract and validate user@host and new_spath */
+	userhost = &conn->req.spath[6];
+	if ((p = index(userhost, '/')) == NULL) {
+		russ_conn_fatal(conn, RUSS_MSG_NO_SERVICE, RUSS_EXIT_FAILURE);
+		exit(0);
+	}
+	for (i = 0; i < hostslist.nhosts; i++) {
+		if (strcmp(userhost, hostslist.hosts[i]) == 0) {
+			break;
+		}
+	}
+	new_spath = strdup(p);
+	p[0] = '\0'; /* terminate userhost */
+	if (i == hostslist.nhosts) {
+		russ_conn_fatal(conn, RUSS_MSG_NO_SERVICE, RUSS_EXIT_FAILURE);
+		exit(0);
+	}
+
+	execute(conn, userhost, new_spath);
+}
+
+void
+svc_net_handler(struct russ_conn *conn) {
+	char	*p, *new_spath, *userhost;
+	int	i;
+
+	/* extract and validate user@host and new_spath */
+	userhost = &conn->req.spath[5];
+	if ((p = index(userhost, '/')) == NULL) {
+		russ_conn_fatal(conn, RUSS_MSG_NO_SERVICE, RUSS_EXIT_FAILURE);
+		exit(0);
+	}
+	new_spath = strdup(p);
+	p[0] = '\0'; /* terminate userhost */
+
+	execute(conn, userhost, new_spath);
+}
+
+/*
+* All ops are passed to handlers for /hid/*, /host/*, and /net/*
+* spaths.
+*/
+void
 master_handler(struct russ_conn *conn) {
 	struct russ_request	*req;
+	int			i;
 
 	req = &(conn->req);
-	if ((strlen(req->spath) > 1) && (strncmp(req->spath, "/", 1) == 0)) {
-		svc_ssh_handler(conn);
-	} else {
-		if (strcmp(req->op, "help") == 0) {
-        		russ_dprintf(conn->fds[1], "%s", HELP);
+	if (strncmp(req->spath, "/hid/", 5) == 0) {
+		svc_hid_handler(conn);
+	} else if (strncmp(req->spath, "/host/", 6) == 0) {
+		svc_host_handler(conn);
+	} else if (strncmp(req->spath, "/net/", 5) == 0) {
+		svc_net_handler(conn);
+	} else if (strcmp(req->op, "execute") == 0) {
+		russ_conn_fatal(conn, RUSS_MSG_NO_SERVICE, RUSS_EXIT_FAILURE);
+	} else if (strcmp(req->op, "help") == 0) {
+        	russ_dprintf(conn->fds[1], "%s", HELP);
+		russ_conn_exit(conn, RUSS_EXIT_SUCCESS);
+	} else if (strcmp(req->op, "list") == 0) {
+		if (strcmp(req->spath, "/") == 0) {
+			russ_dprintf(conn->fds[1], "hid\nhost\nnet\n");
 			russ_conn_exit(conn, RUSS_EXIT_SUCCESS);
+		} else if (strcmp(req->spath, "/hid") == 0) {
+			if (hostslist.nhosts == 0) {
+				russ_conn_fatal(conn, RUSS_MSG_NO_SERVICE, RUSS_EXIT_FAILURE);
+			} else {
+				for (i = 0; i < hostslist.nhosts; i++) {
+					russ_dprintf(conn->fds[1], "%d\n", i);
+				}
+				russ_conn_exit(conn, RUSS_EXIT_SUCCESS);
+			}
+		} else if (strcmp(req->spath, "/host") == 0) {
+			if (hostslist.nhosts == 0) {
+				russ_conn_fatal(conn, RUSS_MSG_NO_SERVICE, RUSS_EXIT_FAILURE);
+			} else {
+				for (i = 0; i < hostslist.nhosts; i++) {
+					russ_dprintf(conn->fds[1], "%s\n", hostslist.hosts[i]);
+				}
+				russ_conn_exit(conn, RUSS_EXIT_SUCCESS);
+			}
+		} else if (strcmp(req->spath, "/net") == 0) {
+			//russ_conn_fatal(conn, RUSS_MSG_UNK_SERVICE, RUSS_EXIT_SUCCESS);
+			russ_conn_fatal(conn, "error: unknown service", RUSS_EXIT_SUCCESS);
 		} else {
-			russ_conn_fatal(conn, RUSS_MSG_BAD_OP, RUSS_EXIT_FAILURE);
+			russ_conn_fatal(conn, RUSS_MSG_NO_SERVICE, RUSS_EXIT_FAILURE);
 		}
+	} else {
+		russ_conn_fatal(conn, RUSS_MSG_BAD_OP, RUSS_EXIT_FAILURE);
 	}
 	exit(0);
 }
@@ -147,9 +268,45 @@ void
 print_usage(char **argv) {
 	fprintf(stderr,
 "usage: rusrv_ssh <conf>\n"
+"                 [options]\n"
 "\n"
-"russ-based server for ssh-based remote connections.\n"
+"russ-based server for ssh-based remote connections. Configuration\n"
+"can be obtained from the conf file if no options are used, otherwise\n"
+"all configuration is taken from the given options.\n"
+"\n"
+"Options:"
+"-c <conf>\n"
+"    Use conf file for server configuration.\n"
+"-h <hostsfile>\n"
+"    Set up /hosts/* and /hid/* services.\n"
+"-p <path>\n"
+"    Use path for service address.\n"
 );
+}
+
+int
+load_hostsfile(char *filename) {
+	char	*line;
+	int	i;
+	size_t	line_size;
+	ssize_t	nbytes;
+	FILE	*f;
+
+	if ((f = fopen(filename, "r")) == NULL) {
+		return -1;
+	}
+	for (i = 0; i < MAX_HOSTS; i++) {
+		line = NULL;
+		if ((nbytes = getline(&line, &line_size, f)) < 0) {
+			break;
+		}
+		if (line[nbytes-1] == '\n') {
+			line[nbytes-1] = '\0';
+		}
+		hostslist.hosts[i] = line;
+	}
+	hostslist.nhosts = i;
+	return 0;
 }
 
 int
@@ -157,25 +314,57 @@ main(int argc, char **argv) {
 	struct russ_listener	*lis;
 	char			*filename, *path;
 	int			mode, uid, gid;
+	char			*arg;
+	int			i;
 
 	signal(SIGCHLD, SIG_IGN);
 	signal(SIGPIPE, SIG_IGN);
 
-	if (argc != 2) {
+	hostslist.nhosts = 0;
+
+	if (argc < 2) {
 		print_usage(argv);
 		exit(-1);
 	}
+	if (argv[1][0] == '-') {
+		/* options */
+		mode = 0666;
+		uid = getuid();
+		gid = getgid();
+		path = NULL;
 
-	filename = argv[1];
-	if ((config = configparser_read(filename)) == NULL) {
-		fprintf(stderr, "error: could not read config file\n");
+		i = 1;
+		while (i < argc) {
+			arg = argv[i++];
+
+			if ((strcmp(arg, "-h") == 0) && (i < argc)) {
+				hostsfilename = argv[i++];
+			} else if ((strcmp(arg, "-p") == 0) && (i < argc)) {
+				path = argv[i++];
+			} else {
+				fprintf(stderr, "error: bad/missing arguments");
+				exit(-1);
+			}
+		}
+	} else if (argc == 2) {
+		/* configuration file */
+		filename = argv[1];
+		if ((config = configparser_read(filename)) == NULL) {
+			fprintf(stderr, "error: could not read config file\n");
+			exit(-1);
+		}
+
+		mode = configparser_getsint(config, "server", "mode", 0600);
+		uid = configparser_getint(config, "server", "uid", getuid());
+		gid = configparser_getint(config, "server", "gid", getgid());
+		path = configparser_get(config, "server", "path", NULL);
+	}
+
+	if ((hostsfilename != NULL) && (load_hostsfile(hostsfilename) < 0)) {
+		fprintf(stderr, "error: could not load hosts file\n");
 		exit(-1);
 	}
 
-	mode = configparser_getsint(config, "server", "mode", 0600);
-	uid = configparser_getint(config, "server", "uid", getuid());
-	gid = configparser_getint(config, "server", "gid", getgid());
-	path = configparser_get(config, "server", "path", NULL);
 	if ((lis = russ_announce(path, mode, uid, gid)) == NULL) {
 		fprintf(stderr, "error: cannot announce service\n");
 		exit(-1);
