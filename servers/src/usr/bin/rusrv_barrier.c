899e97acaa997cc8abcde827497371294c1bd6ce
John Marshall <johnm.dev@gmail.com>
2012-07-22 14:35:24 -0400

    Rename config variable to conf (in servers)

--- servers/src/usr/bin/rusrv_barrier.c
+++ servers/src/usr/bin/rusrv_barrier.c
@@ -43,7 +43,7 @@
 #include <sys/stat.h>
 #include <time.h>
 
-#include "russ_configparser.h"
+#include "russ_confparser.h"
 #include "russ.h"
 
 #define MAX( a,b ) (((a) > (b)) ? (a) : (b))
@@ -64,7 +64,7 @@ struct barrier {
 };
 
 /* globals */
-struct russ_configparser	*config;
+struct russ_confparser	*conf;
 struct barrier		*barrier;
 
 /*
@@ -473,7 +473,7 @@ print_usage(char **argv) {
 
 	prog_name = basename(argv[0]);
 	fprintf(stderr,
-"usage: %s <config_filename>\n"
+"usage: %s <conf_filename>\n"
 "\n"
 "Front-end barrier service. Instantiates backend barrier service.\n",
 		prog_name);
@@ -495,15 +495,15 @@ main(int argc, char **argv) {
 	}
 
 	filename = argv[1];
-	if ((config = russ_configparser_read(filename)) == NULL) {
-		fprintf(stderr, "error: could not read config file\n");
+	if ((conf = russ_confparser_read(filename)) == NULL) {
+		fprintf(stderr, "error: could not read conf file\n");
 		exit(-1);
 	}
 
-	mode = russ_configparser_getsint(config, "server", "mode", 0600);
-	uid = russ_configparser_getint(config, "server", "uid", getuid());
-	gid = russ_configparser_getint(config, "server", "gid", getgid());
-	saddr = russ_configparser_get(config, "server", "path", NULL);
+	mode = russ_confparser_getsint(conf, "server", "mode", 0600);
+	uid = russ_confparser_getint(conf, "server", "uid", getuid());
+	gid = russ_confparser_getint(conf, "server", "gid", getgid());
+	saddr = russ_confparser_get(conf, "server", "path", NULL);
 	if ((lis = russ_announce(saddr, mode, uid, gid)) == NULL) {
 		fprintf(stderr, "error: cannot announce service\n");
 		exit(-1);
