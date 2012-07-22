ab0f7aea6b2edb9b55b29071bff04d73f28803f2
John Marshall <John.Marshall@ec.gc.ca>
2012-07-22 16:45:02 -0400

    (RUSSNG-82,RUSSNG-88) Add russ_conf_init to provide standardize server arg handling

--- library/src/usr/include/russ_conf.h
+++ library/src/usr/include/russ_conf.h
@@ -25,11 +25,13 @@ struct russ_conf {
 	int				len, cap;
 };
 
-
 struct russ_conf *russ_conf_new(void);
 void russ_conf_free(struct russ_conf *);
+struct russ_conf *russ_conf_init(int *, char **, void (*)(char **));
+
 int russ_conf_add_section(struct russ_conf *, char *);
 int russ_conf_has_section(struct russ_conf *, char *);
+
 char *russ_conf_get(struct russ_conf *, char *, char *, char *);
 long russ_conf_getint(struct russ_conf *, char *, char *, long);
 double russ_conf_getfloat(struct russ_conf *, char *, char *, double);
@@ -40,6 +42,7 @@ int russ_conf_remove_option(struct russ_conf *, char *, char *);
 int russ_conf_remove_section(struct russ_conf *, char *);
 int russ_conf_set(struct russ_conf *, char *, char *, char *);
 int russ_conf_set2(struct russ_conf *, char *, char *, char *);
+
 void russ_conf_sarray0_free(char **);
 char **russ_conf_sections(struct russ_conf *);
 int russ_conf_write(struct russ_conf *, FILE *);
