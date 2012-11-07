c0754d18f62441eeb163534ed871ef8636f6a983
John Marshall <johnm.dev@gmail.com>
2012-11-07 00:10:08 -0500

    (RUSSNG-166) Support ++ alias for ~/.russ

--- library/src/usr/include/russ.h
+++ library/src/usr/include/russ.h
@@ -155,6 +155,7 @@ typedef void (*russ_req_handler)(struct russ_conn *);
 /* addr.c */
 struct russ_target *russ_find_service_target(char *);
 char *russ_resolve_addr(char *);
+char *russ_resolve_addr_uid(char *, uid_t *);
 
 /* conn.c */
 int russ_conn_accept(struct russ_conn *, int, int *, int *);
