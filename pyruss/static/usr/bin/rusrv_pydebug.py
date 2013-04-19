955bbf88cf2fbe14fd41b7751e60d8938ed96b67
John Marshall <johnm.dev@gmail.com>
2013-04-19 00:43:11 -0400

    Revamped/updated pydebug.py python-based debug server

--- pyruss/static/usr/bin/rusrv_pydebug.py
+++ pyruss/static/usr/bin/rusrv_pydebug.py
@@ -3,6 +3,7 @@
 # rusrv_pydebug
 
 # system imports
+import logging
 import os
 import signal
 import sys
@@ -10,13 +11,35 @@ import time
 import traceback
 
 #
-from pyruss import Server, ConfigParser
-from pyruss import ServiceTree
+import pyruss
+from pyruss import Conf
+from pyruss.server import ServiceNode, Server
 
 HELP = """\
-Threaded, Python-based debug server.
+Forking, Python-based debug server.
 
-/
+Provides services useful for debugging. Unless otherwise stated,
+stdin, stdout, and stderr all refer to the file descriptor triple
+that is returned from a russ_dial call.
+
+/chargen
+    Character generator outputting to stdout; follows the RPC 864
+    the RFC 864 protocol sequence.
+
+/conn
+    Outputs russ connection information.
+
+/daytime
+    Outputs the date and time to stdout.
+
+/echo
+    Simple echo service; receives from stdin and outputs to stdout.
+
+/env
+    Outputs environ entries to stdout.
+
+/request
+    Outputs the request information to the server stdout.
 """
 
 def setup_logger(config):
@@ -29,126 +52,136 @@ def setup_logger(config):
     logger = logging.getLogger()
 
 CHARGEN_CHARS = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ ";
-class PydebugServiceTree(ServiceTree):
+class ServiceTree:
 
     def __init__(self):
-        ServiceTree.__init__(self)
-        self.add("/chargen", ["execute"], self.svc_chargen)
-        self.add("/conn", ["execute"], self.svc_conn)
-        self.add("/daytime", ["execute"], self.svc_daytime)
-        self.add("/echo", ["execute"], self.svc_echo)
-        self.add("/env", ["execute"], self.svc_env)
-        self.add("/request", ["execute"], self.svc_request)
+        self.root = ServiceNode.new("/", self.svc_root)
+        self.root.add("chargen", self.svc_chargen)
+        self.root.add("conn", self.svc_conn)
+        self.root.add("daytime", self.svc_daytime)
+        self.root.add("echo", self.svc_echo)
+        self.root.add("env", self.svc_env)
+        self.root.add("request", self.svc_request)
 
-    def _handler(self, conn):
-        print "AAA"
+    def svc_root(self, sess):
+        conn = sess.get_conn()
         req = conn.get_request()
-        print "handler op (%s) spath (%s)" % (req.op, req.spath)
-
-    def fallback_handler(self, conn):
-        req = conn.get_request()
-        if req.op == "help":
+        if req.opnum == pyruss.RUSS_OPNUM_HELP:
             os.write(conn.get_fd(1), HELP)
-        else:
-            ServiceTree.fallback_handler(self, conn)
+            conn.exit(pyruss.RUSS_EXIT_SUCCESS)
+            conn.close()
 
-    def svc_chargen(self, conn):
-
-        fd = conn.get_fd(1)
-        off = 0
-        while True:
-            os.write(fd, CHARGEN_CHARS[off:off+72]+"\n")
-            off += 1
-            if off > 94:
-                off = 0
-            time.sleep(0.1)
-        return 0
-
-    def svc_conn(self, conn):
-        fd = conn.get_fd(1)
-        try:
-            creds = conn.get_creds()
-            os.write(fd, "uid (%s)\ngid (%s)\npid (%d)\n" % (creds.uid, creds.gid, creds.pid))
-        except:
-            traceback.print_exc()
-        return 0
-
-    def svc_daytime(self, conn):
-        fd = conn.get_fd(1)
-        s = time.strftime("%A, %B %d, %Y %T-%Z", time.gmtime(time.time()))
-        os.write(fd, s+"\n")
-        return 0
-
-    def svc_echo(self, conn):
-        infd = conn.get_fd(0)
-        outfd = conn.get_fd(1)
-        while True:
-            s = os.read(infd, 128)
-            if s == "":
-                break
-            os.write(outfd, s)
-        return 0
-
-    def svc_env(self, conn):
-        fd = conn.get_fd(1)
-        for key, value in os.environ.items():
-            os.write("%s=%s\n" % (key, value))
-        return 0
-
-    def svc_request(self, conn):
-        fd = conn.get_fd(1)
+    def svc_chargen(self, sess):
+        conn = sess.get_conn()
         req = conn.get_request()
-        os.write(fd, "protocol string (%s)\n" % req.protocol_string)
-        os.write(fd, "spath (%s)\n" % req.spath)
-        os.write(fd, "op (%s)\n" % req.op)
+        if req.opnum == pyruss.RUSS_OPNUM_EXECUTE:
+            fd = conn.get_fd(1)
+            off = 0
+            while True:
+                os.write(fd, CHARGEN_CHARS[off:off+72]+"\n")
+                off += 1
+                if off > 94:
+                    off = 0
+                time.sleep(0.1)
+            conn.exit(pyruss.RUSS_EXIT_SUCCESS)
+            conn.close()
 
-        attrs = conn.get_request_attrs()
-        if not attrs:
-            os.write(fd, "attrv (NULL)\n")
-        else:
-            for i, (key, value) in enumerate(attrs):
-                os.write(fd, "attrv[%d] (%s=%s)\n" % (i, key, value))
+    def svc_conn(self, sess):
+        conn = sess.get_conn()
+        req = conn.get_request()
+        if req.opnum == pyruss.RUSS_OPNUM_EXECUTE:
+            fd = conn.get_fd(1)
+            try:
+                creds = conn.get_creds()
+                os.write(fd, "uid (%s)\ngid (%s)\npid (%d)\n" % (creds.uid, creds.gid, creds.pid))
+                conn.exit(pyruss.RUSS_EXIT_SUCCESS)
+                conn.close()
+            except:
+                traceback.print_exc()
 
-        args = conn.get_request_args()
-        if not args:
-            os.write(fd, "argv (NULL)\n")
-        else:
-            for i, value in enumerate(args):
-                os.write(fd, "argv[%d] (%s)\n" % (i, value))
+    def svc_daytime(self, sess):
+        conn = sess.get_conn()
+        req = conn.get_request()
+        if req.opnum == pyruss.RUSS_OPNUM_EXECUTE:
+            fd = conn.get_fd(1)
+            s = time.strftime("%A, %B %d, %Y %T-%Z", time.gmtime(time.time()))
+            os.write(fd, s+"\n")
+            conn.exit(pyruss.RUSS_EXIT_SUCCESS)
+            conn.close()
+
+    def svc_echo(self, sess):
+        conn = sess.get_conn()
+        req = conn.get_request()
+        if req.opnum == pyruss.RUSS_OPNUM_EXECUTE:
+            infd = conn.get_fd(0)
+            outfd = conn.get_fd(1)
+            while True:
+                s = os.read(infd, 128)
+                if s == "":
+                    break
+                os.write(outfd, s)
+            conn.exit(pyruss.RUSS_EXIT_SUCCESS)
+            conn.close()
+
+    def svc_env(self, sess):
+        conn = sess.get_conn()
+        req = conn.get_request()
+        if req.opnum == pyruss.RUSS_OPNUM_EXECUTE:
+            fd = conn.get_fd(1)
+            for key, value in os.environ.items():
+                os.write(fd, "%s=%s\n" % (key, value))
+            conn.exit(pyruss.RUSS_EXIT_SUCCESS)
+            conn.close()
+
+    def svc_request(self, sess):
+        conn = sess.get_conn()
+        req = conn.get_request()
+        if req.opnum == pyruss.RUSS_OPNUM_EXECUTE:
+            fd = conn.get_fd(1)
+            req = conn.get_request()
+            os.write(fd, "protocol string (%s)\n" % req.protocol_string)
+            os.write(fd, "spath (%s)\n" % req.spath)
+            os.write(fd, "op (%s)\n" % req.op)
+
+            attrs = conn.get_request_attrs()
+            if not attrs:
+                os.write(fd, "attrv (NULL)\n")
+            else:
+                for i, (key, value) in enumerate(attrs):
+                    os.write(fd, "attrv[%d] (%s=%s)\n" % (i, key, value))
+
+            args = conn.get_request_args()
+            if not args:
+                os.write(fd, "argv (NULL)\n")
+            else:
+                for i, value in enumerate(args):
+                    os.write(fd, "argv[%d] (%s)\n" % (i, value))
+
+            conn.exit(pyruss.RUSS_EXIT_SUCCESS)
+            conn.close()
 
 if __name__ == "__main__":
     global config, logger
 
-    args = sys.argv[1:]
-    if len(args) < 1:
+    try:
+        if len(sys.argv) < 2:
+            raise Exception
+        config = Conf()
+        config.init(sys.argv[1:])
+    except:
         sys.stderr.write("error: bad/missing arguments\n")
-        sys.exit(-1)
-
-    signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+        sys.exit(1)
 
     try:
-        if 1:
-            mode = 0666
-            uid = os.getuid()
-            gid = os.getgid()
-            saddr = args[0]
-        else:
-            config_filename = args[0]
-            config = ConfigParser()
-            config.read(config_filename)
+        setup_logger(config)
 
-            setup_logger(config)
-            logger.info("starting server")
-
-            mode = int(config.get("server", "mode", "0666"), 8)
-            uid = config.getint("server", "uid", os.getuid())
-            gid = config.getint("server", "gid", os.getgid())
-            saddr = config.get("server", "path")
-
-        #s = Server(PydebugServiceTree(), "thread")
-        s = Server(PydebugServiceTree(), "fork")
-        s.announce(saddr, mode, uid, gid)
-        s.loop()
+        svr = Server.new(ServiceTree().root, pyruss.RUSS_SVR_TYPE_FORK)
+        svr.announce(config.get("server", "path"),
+            int(config.get("server", "mode", "0666"), 8),
+            config.getint("server", "uid", os.getuid()),
+            config.getint("server", "gid", os.getgid()))
+        svr.loop()
+        #s = Server(PydebugServiceTree(), "fork")
     except SystemExit:
         raise
     except:
