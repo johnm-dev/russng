45e919dd8f82b66b187b9cf24c872aec2b27f5a7
John Marshall <johnm.dev@gmail.com>
2013-10-18 11:19:13 -0400

    Should return NULL on failure

--- library/src/usr/lib/server.c
+++ library/src/usr/lib/server.c
@@ -76,7 +76,7 @@ russ_svr_accept(struct russ_svr *self, russ_deadline deadline) {
 struct russ_lis *
 russ_svr_announce(struct russ_svr *self, char *saddr, mode_t mode, uid_t uid, gid_t gid) {
 	if ((self->saddr = strdup(saddr)) == NULL) {
-		return;
+		return NULL;
 	}
 	self->mode = mode;
 	self->uid = uid;
