937fe39523a107ce68cac00cc8156ece8574cec7
John Marshall <johnm.dev@gmail.com>
2013-12-29 21:24:29 -0500

    Add snprintf_test and str_test programs

---
+++ tests/src/usr/bin/str_test.c
@@ -0,0 +1,24 @@
+/*
+* str_test.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <russ.h>
+
+int
+main(int argc, char **argv) {
+	char	*spath = "/a/bc/def";
+	char	*s;
+	int	i;
+
+	for (i = 0; i < 10; i++) {
+		s = russ_str_get_comp(spath, '/', i);
+		if (s == NULL) {
+			break;
+		}
+		printf("spath (%s) i (%d) s (%s)\n", spath, i, s);
+		free(s);
+	};
+}
