2621711aee3d47a75e2e87c0b019c8944467bd3d
John Marshall <johnm.dev@gmail.com>
2019-01-17 05:11:53 -0500

    (RUSSNG-937) Add docstring to pyruss/__init__.py

--- pyruss/static/usr/lib/russng/pyruss/__init__.py
+++ pyruss/static/usr/lib/russng/pyruss/__init__.py
@@ -2,6 +2,18 @@
 #
 #  pyruss/__init__.py
 
+"""pyruss is a Python implementation of RUSS, based on the russng C
+implementation.
+
+RUSS is an alternative to HTTP/web technologies for services running
+on UNIX/Linux.
+
+RUSS is a protocol and framework for building service-oriented
+servers using UNIX/Domain sockets.
+
+See https://expl.info/display/RUSS .
+"""
+
 from .bindings import *
 from .conf import *
 from .base import *
