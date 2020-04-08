644a5fba9f5e19040494390635054a2291d8e031
John Marshall <johnm.dev@gmail.com>
2020-04-08 07:12:26 -0400

    (RUSSNG-1022) Tweak/cleanup rumpirun

--- tools/static/usr/lib/russng/tools/rumpirun.sh
+++ tools/static/usr/lib/russng/tools/rumpirun.sh
@@ -28,6 +28,10 @@
 # RUMPIRUN_MPIRUN_IMPL (opt)
 # RUMPIRUN_SHELL (opt)
 
+echoe() {
+	echo "$*" 1>&2
+}
+
 function print_usage {
 	echo "usage: ${PROG_NAME} [options] ...
 
@@ -56,38 +60,38 @@ function is_mpich {
 
 PROG_NAME=$(basename $0)
 
-export RUMPIRUN_MPIRUN=${RUMPIRUN_MPIRUN:-mpirun}
-export RUMPIRUN_LAUNCHER=${RUMPIRUN_LAUNCHER:-rurun}
+export RUMPIRUN_MPIRUN="${RUMPIRUN_MPIRUN:-mpirun}"
+export RUMPIRUN_LAUNCHER="${RUMPIRUN_LAUNCHER:-rurun}"
 export RUMPIRUN_SHELL=""
 export RUMPIRUN_TARGETSFILE="${RUMPIRUN_TARGETSFILE:-${RUMPIRUN_PNET_TARGETSFILE}}"
 
 # get absolute path
-RUMPIRUN_MPIRUN=$(which ${RUMPIRUN_MPIRUN} 2> /dev/null)
+RUMPIRUN_MPIRUN=$(which "${RUMPIRUN_MPIRUN}" 2> /dev/null)
 if [ -z "${RUMPIRUN_MPIRUN}" ]; then
-	echo "error: cannot find mpirun" 1>&2
+	echoe "error: cannot find mpirun"
 	exit 1
 fi
 
 # identify implementation
 if [ -n "${RUMPIRUN_MPIRUN_IMPL}" ]; then
-	echo "info: using mpirun implementation override (${RUMPIRUN_MPIRUN_IMPL})" 1>&2
-elif is_openmpi ${RUMPIRUN_MPIRUN}; then
+	echoe "info: using mpirun implementation override (${RUMPIRUN_MPIRUN_IMPL})"
+elif is_openmpi "${RUMPIRUN_MPIRUN}"; then
 	export RUMPIRUN_MPIRUN_IMPL="openmpi"
-elif is_mpich ${RUMPIRUN_MPIRUN}; then
+elif is_mpich "${RUMPIRUN_MPIRUN}"; then
 	export RUMPIRUN_MPIRUN_IMPL="mpich"
 else
-	echo "error: unknown implementation of mpirun"
+	echoe "error: unknown implementation of mpirun"
 	exit 1
 fi
 
-echo "info: MPI implementation (${RUMPIRUN_MPIRUN_IMPL})" 1>&2
+echoe "info: MPI implementation (${RUMPIRUN_MPIRUN_IMPL})"
 
 # setup by implementation
 case "${RUMPIRUN_MPIRUN_IMPL}" in
 mpich)
-	export HYDRA_BOOTSTRAP=rsh
+	export HYDRA_BOOTSTRAP="rsh"
 	export HYDRA_BOOTSTRAP_EXEC="${RUMPIRUN_LAUNCHER}"
-	export HYDRA_LAUNCHER=rsh
+	export HYDRA_LAUNCHER="rsh"
 	export HYDRA_LAUNCHER_EXEC="${RUMPIRUN_LAUNCHER}"
 	export HYDRA_HOST_FILE="${RUMPIRUN_HOSTFILE}"
 	export HYDRA_LAUNCHER_AUTOFORK=0
@@ -103,7 +107,7 @@ openmpi)
 	MPIRUN_ARGS=""
 	;;
 *)
-	echo "error: unknown implementation (${RUMPIRUN_MPIRUN_IMPL}) of mpirun" 1>&2
+	echoe "error: unknown implementation (${RUMPIRUN_MPIRUN_IMPL}) of mpirun"
 	exit 1
 	;;
 esac
@@ -118,7 +122,7 @@ if [ -z "${RUMPIRUN_PNET_ADDR}" ]; then
 		trap "kill -HUP -$$" EXIT
 	else
 		# fallback to RURUN_PNET_ADDR for backward compatibility ... for now
-		echo "warning: RUMPIRUN_PNET_ADDR is not set; falling back to RURUN_PNET_ADDR" 1>&2
+		echoe "warning: RUMPIRUN_PNET_ADDR is not set; falling back to RURUN_PNET_ADDR"
 		export RUMPIRUN_PNET_ADDR="${RURUN_PNET_ADDR}"
 	fi
 fi
