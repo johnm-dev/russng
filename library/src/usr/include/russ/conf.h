e3d2e513ee4fc73ba3b5050509fb4e4a5d6d3015
John Marshall <johnm.dev@gmail.com>
2020-04-16 09:09:07 -0400

    (RUSSNG-1049) Reorder russ_conf.h alphabetically

--- library/src/usr/include/russ/conf.h
+++ library/src/usr/include/russ/conf.h
@@ -56,31 +56,28 @@ struct russ_conf {
 
 int russ_is_conffile(char *);
 
-struct russ_conf *russ_conf_new(void);
-struct russ_conf *russ_conf_free(struct russ_conf *);
-struct russ_conf *russ_conf_dup(struct russ_conf *);
-struct russ_conf *russ_conf_load(int *, char **);
-struct russ_conf *russ_conf_init(int *, char **);
-
 int russ_conf_add_section(struct russ_conf *, const char *);
+struct russ_conf *russ_conf_dup(struct russ_conf *);
 int russ_conf_dup_section(struct russ_conf *, const char *, const char *);
-int russ_conf_has_section(struct russ_conf *, const char *);
-int russ_conf_has_option(struct russ_conf *, const char *, const char *);
-
+struct russ_conf *russ_conf_free(struct russ_conf *);
 char *russ_conf_get(struct russ_conf *, const char *, const char *, const char *);
 long russ_conf_getint(struct russ_conf *, const char *, const char *, long);
 double russ_conf_getfloat(struct russ_conf *, const char *, const char *, double);
 long russ_conf_getsint(struct russ_conf *, const char *, const char *, long);
+int russ_conf_has_option(struct russ_conf *, const char *, const char *);
+int russ_conf_has_section(struct russ_conf *, const char *);
+struct russ_conf *russ_conf_init(int *, char **);
+struct russ_conf *russ_conf_load(int *, char **);
+struct russ_conf *russ_conf_new(void);
 char **russ_conf_options(struct russ_conf *, const char *);
 int russ_conf_read(struct russ_conf *, const char *);
 int russ_conf_readfd(struct russ_conf *, int);
 int russ_conf_remove_option(struct russ_conf *, const char *, const char *);
 int russ_conf_remove_section(struct russ_conf *, const char *);
-int russ_conf_set(struct russ_conf *, const char *, const char *, const char *);
-int russ_conf_set2(struct russ_conf *, const char *, const char *, const char *);
-
 void russ_conf_sarray0_free(char **);
 char **russ_conf_sections(struct russ_conf *);
+int russ_conf_set(struct russ_conf *, const char *, const char *, const char *);
+int russ_conf_set2(struct russ_conf *, const char *, const char *, const char *);
 int russ_conf_update(struct russ_conf *, struct russ_conf *);
 int russ_conf_write(struct russ_conf *, char *);
 int russ_conf_writefd(struct russ_conf *, int);
