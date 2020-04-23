a6bf9fb2d16e5aed517dc1de551d72f42f7db54e
John Marshall <johnm.dev@gmail.com>
2020-04-23 16:24:33 -0400

    (RUSSNG-1076) Reduce the number of dial-oriented convenience functions

--- library/src/usr/include/russ/russ.h
+++ library/src/usr/include/russ/russ.h
@@ -296,16 +296,6 @@ int russ_dialv_wait_timeout(int, const char *, const char *, char **, char **, i
 int russ_dialv_wait_inouterr(russ_deadline, const char *, const char *, char **, char **, int *, struct russ_buf **);
 int russ_dialv_wait_inouterr_timeout(int, const char *, const char *, char **, char **, int *, struct russ_buf **);
 int russ_dialv_wait_inouterr3(russ_deadline, const char *, const char *, char **, char **, int *, struct russ_buf *, struct russ_buf *, struct russ_buf *);
-struct russ_cconn *russ_execv(russ_deadline, const char *, char **, char **);
-struct russ_cconn *russ_execv_timeout(int, const char *, char **, char **);
-int russ_execv_wait(russ_deadline, const char *, char **, char **, int *);
-int russ_execv_wait_timeout(int, const char *, char **, char **, int *);
-int russ_execv_wait_inouterr(russ_deadline, const char *, char **, char **, int *, struct russ_buf **);
-int russ_execv_wait_inouterr_timeout(int, const char *, char **, char **, int *, struct russ_buf **);
-struct russ_cconn *russ_execl(russ_deadline, const char *, char **, ...);
-struct russ_cconn *russ_help(russ_deadline, const char *);
-struct russ_cconn *russ_info(russ_deadline, const char *);
-struct russ_cconn *russ_list(russ_deadline, const char *);
 struct russ_svr *russ_init(struct russ_conf *);
 
 /* env.c */
